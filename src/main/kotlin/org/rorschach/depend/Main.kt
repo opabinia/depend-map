94fbf8b181cc19f487409882fa410dc4f41b4a99
opabinia <opabinia.burgess@gmail.com>
2015-11-30 22:56:07 +0900

    implement loop detection

--- src/main/kotlin/org/rorschach/depend/Main.kt
+++ src/main/kotlin/org/rorschach/depend/Main.kt
@@ -1,7 +1,62 @@
 package org.rorschach.depend
 
-public fun main(args: Array<String>) {
-    val depend = loadDependMap("declare-depend.txt")
+import java.io.File
+import java.text.MessageFormat
+import java.util.*
 
-    for(it in depend) println(it)
+public fun main(args: Array<String>) {
+    for (i in loadDependGraph("declare-depend.txt")) {
+        println(MessageFormat.format("{0} -> {1}", i.key, i.value))
+    }
+    print(isGraphLooped(loadDependGraph("declare-depend.txt"), "c"))
+}
+
+public fun loadDependGraph(path: String): Map<String, List<String>> {
+    val result = HashMap<String, List<String>>()
+    File(path).forEachLine {
+        val e = it.replace(" ", "").split(":")
+        val target = e[0]
+
+        val d = ArrayList<String>()
+        if (e.size >= 2) {
+            for (i in e[1].split(",")) {
+                d.add(i)
+            }
+        }
+        result[target] = d
+    }
+
+    return result
+}
+
+public fun isGraphLooped(graph: Map<String, List<String>>, node: String): Boolean {
+    var path: List<List<String>> = listOf(listOf(node))
+
+    while(path.size != 0) {
+        val newpath: MutableList<List<String>> = ArrayList<List<String>>()
+
+        for(i in path) {
+            val next = graph[i.last()] ?: throw NullPointerException()
+            if (next.size == 0) {
+                for(j in i) {
+                    print(j);print(" -> ")
+                }
+                println("[end]")
+            } else {
+                for(j in next) {
+                    if(i.any{it.equals(j)}) {
+                        for(k in i) {
+                            print(k);print(" -> ")
+                        }
+                        println(MessageFormat.format("[loop to {0}]", j))
+                        return false
+                    } else {
+                        newpath.add(i + listOf(j))
+                    }
+                }
+            }
+            path = newpath
+        }
+    }
+    return true
 }
